cbc8dc45d5526684a4b2727ddad996989a52c4bf
kongzhidea <563150913@qq.com>
2016-07-21 16:03:32 +0800

    mod: 反射 获取 静态变量， 获取方法返回值List，map里面的泛型

--- utils/src/main/java/com/kk/utils/reflect/GenericUtils.java
+++ utils/src/main/java/com/kk/utils/reflect/GenericUtils.java
@@ -27,6 +27,38 @@ public class GenericUtils {
 
     private static final Class[] EMPTY_CLASSES = new Class[0];
 
+
+    /**
+     * 从参数, 返回值, 基类的: Generic 类型信息获取传入的实际类信息。 获取list，map里面的泛型
+     * <p/>
+     * 调用方式：getActualClass(method.getGenericReturnType())
+     *
+     * @param genericType - Generic 类型信息
+     * @return 实际类信息
+     */
+    public static Class<?>[] getActualClass(Type genericType) {
+
+        if (genericType instanceof ParameterizedType) {
+
+            Type[] actualTypes = ((ParameterizedType) genericType).getActualTypeArguments();
+            Class<?>[] actualClasses = new Class<?>[actualTypes.length];
+
+            for (int i = 0; i < actualTypes.length; i++) {
+                Type actualType = actualTypes[i];
+                if (actualType instanceof Class<?>) {
+                    actualClasses[i] = (Class<?>) actualType;
+                } else if (actualType instanceof GenericArrayType) {
+                    Type componentType = ((GenericArrayType) actualType).getGenericComponentType();
+                    actualClasses[i] = Array.newInstance((Class<?>) componentType, 0).getClass();
+                }
+            }
+
+            return actualClasses;
+        }
+
+        return EMPTY_CLASSES;
+    }
+
     /**
      * 返回类型List<E>、Map<K,V>中的E、K、V
      * <p/>
