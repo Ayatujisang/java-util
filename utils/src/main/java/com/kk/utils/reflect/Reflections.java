97c3c16271fc0caccfda9e08239f7439610900e3
zhihui.kzh <zhihui.kzh@alibaba-inc.com>
2017-08-29 16:21:05 +0800

    MOD:up

--- utils/src/main/java/com/kk/utils/reflect/Reflections.java
+++ utils/src/main/java/com/kk/utils/reflect/Reflections.java
@@ -403,4 +403,43 @@ public class Reflections {
         Object value = method.invoke(object);
         return value;
     }
+
+    private static final char PACKAGE_SEPARATOR_CHAR = '.';
+
+    /**
+     * The shortcut to {@link #simpleClassName(Class) simpleClassName(o.getClass())}.
+     */
+    public static String simpleClassName(Object o) {
+        if (o == null) {
+            return "null_object";
+        } else {
+            return simpleClassName(o.getClass());
+        }
+    }
+
+    /**
+     * 获取类名，不含包名，支持匿名内部类。
+     *
+     * Generates a simplified name from a {@link Class}.  Similar to {@link Class#getSimpleName()}, but it works fine
+     * with anonymous classes.
+     */
+    public static String simpleClassName(Class<?> clazz) {
+        String className = checkNotNull(clazz, "clazz").getName();
+        final int lastDotIdx = className.lastIndexOf(PACKAGE_SEPARATOR_CHAR);
+        if (lastDotIdx > -1) {
+            return className.substring(lastDotIdx + 1);
+        }
+        return className;
+    }
+
+    /**
+     * Checks that the given argument is not null. If it is, throws {@link NullPointerException}.
+     * Otherwise, returns the argument.
+     */
+    public static <T> T checkNotNull(T arg, String text) {
+        if (arg == null) {
+            throw new NullPointerException(text);
+        }
+        return arg;
+    }
 }
