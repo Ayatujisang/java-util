915418e02b19571a1d76a1673c65939486533063
zhihui.kzh <zhihui.kzh@alibaba-inc.com>
2017-09-06 17:43:36 +0800

    netty4 http file

---
+++ utils/src/test/java/com/kk/content_type/ContentTypeTest.java
@@ -0,0 +1,24 @@
+package com.kk.content_type;
+
+import org.junit.Test;
+
+import javax.activation.MimetypesFileTypeMap;
+import java.io.File;
+
+/**
+ * 根据文件名称，获取在http中显示的 contentType信息， 如:text/html等。
+ *
+ * 需要将 mime.types 放到 resources/META-INF/mime.types位置。  样例见： netty4-demo。
+ *
+ * @auther zhihui.kzh
+ * @create 6/9/1710:18
+ */
+public class ContentTypeTest {
+
+    @Test
+    public void test() {
+        File file = new File("~/test/s.html");
+        MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        System.out.println(mimeTypesMap.getContentType(file.getPath())); // application/octet-stream
+    }
+}
